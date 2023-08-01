7c8a702f4811fc832a253d870295f96ddda2faeb
Chinyi Pam <chinyi.pam@gmail.com>
2023-08-01 14:53:59 +0800

    structure and style markdown editor

--- pages/markdown.vue
+++ pages/markdown.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <h2>markdown</h2>
+    <main class="my-24">
+      <div class="container">
+        <h2>Markdown Editor</h2>
+
+        <div class="flex min-h-screen">
+          <textarea class="w-1/2 bg-white p-4 outline-0">gaga</textarea>
+          <article class="w-1/2 bg-stone-100 p-4">shis</article>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
