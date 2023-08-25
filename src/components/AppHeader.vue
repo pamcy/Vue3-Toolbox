74c3e4716eb5bd8937453f1a5bc8ebdfd5f0dacf
Chinyi Pam <chinyi.pam@gmail.com>
2023-08-25 17:01:37 +0800

    refactor navigation menu items to use dynamic rendering

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,10 +3,9 @@
     <div class="container">
       <nav>
         <ul class="flex">
-          <li class="mx-3"><RouterLink to="/">Home</RouterLink></li>
-          <li class="mx-3"><RouterLink to="/calendar">Calendar</RouterLink></li>
-          <li class="mx-3"><RouterLink to="/markdown">Markdown</RouterLink></li>
-          <li class="mx-3"><RouterLink to="/carousel">Carousel</RouterLink></li>
+          <li v-for="item in menuItems" :key="item.title" class="mx-3">
+            <RouterLink :to="item.path">{{ item.title }}</RouterLink>
+          </li>
         </ul>
       </nav>
     </div>
@@ -15,4 +14,24 @@
 
 <script setup>
 import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+const menuItems = ref([
+  {
+    title: 'Home',
+    path: '/'
+  },
+  {
+    title: 'Calendar',
+    path: '/calendar'
+  },
+  {
+    title: 'Markdown',
+    path: '/markdown'
+  },
+  {
+    title: 'Carousel',
+    path: 'carousel'
+  }
+])
 </script>
