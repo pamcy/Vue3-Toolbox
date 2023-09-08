1050aeeadef632f9d067cda75981bb0fa49f1c49
Chinyi Pam <chinyi.pam@gmail.com>
2023-09-08 11:26:13 +0800

    add authentication status check on mount

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <script setup>
+import { auth } from './utils/firebase'
+import { onAuthStateChanged } from 'firebase/auth'
 import { RouterView } from 'vue-router'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+
 import AppHeader from './components/AppHeader.vue'
 import LoginModal from './components/LoginModal.vue'
 
 const isLoginModalOpen = ref(false)
+const isLoggedIn = ref(false)
+const authUser = ref({})
+
+onMounted(() => {
+  // Get the currently signed-in user
+  // https://firebase.google.com/docs/auth/web/manage-users#get_the_currently_signed-in_user
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      isLoggedIn.value = true
+      authUser.value = user
+    } else {
+      isLoggedIn.value = false
+      authUser.value = {}
+    }
+  })
+})
 </script>
 
 <template>
